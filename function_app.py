b22c2806f2e782e5d9b1553af0c882832291d008
Joseph Masengesho <jmasengesho@microsoft.com>
2025-03-01 19:46:58 -0600

    added open ai

--- function_app.py
+++ function_app.py
@@ -1,18 +1,23 @@
-import datetime
-import json
-import os
-import time
-
-import azure.durable_functions as df
+import random
 import azure.functions as func
-import numpy as np
-from azure.data.tables import TableServiceClient, UpdateMode
-from azure.identity import DefaultAzureCredential
+import azure.durable_functions as df
 from azure.monitor.opentelemetry import configure_azure_monitor
-from azure.storage.queue import (BinaryBase64DecodePolicy,
-                                 BinaryBase64EncodePolicy, QueueServiceClient)
 from opentelemetry import trace
 from opentelemetry.trace import SpanKind
+import numpy as np
+import time
+import datetime
+from azure.storage.queue import (
+        QueueServiceClient,
+        BinaryBase64EncodePolicy,
+        BinaryBase64DecodePolicy
+)
+from azure.data.tables import TableServiceClient, UpdateMode
+import os
+from azure.identity import DefaultAzureCredential, get_bearer_token_provider
+import json
+import uuid
+from openai import AzureOpenAI
 
 # Configure Azure Monitor with OpenTelemetry
 configure_azure_monitor()
@@ -116,7 +121,7 @@ def create_message_timer(mytimer: func.TimerRequest) -> None:
         # Use DefaultAzureCredential to authenticate with managed identity
         credential = DefaultAzureCredential()
 
-        storage_account_name = os.getenv("STORAGE_ACCOUNT_NAME", "matrixmathconsumptionfun")
+        storage_account_name = os.getenv("STORAGE_ACCOUNT_NAME", "azdurablefunc001")
         storage_table_name = os.getenv("STORAGE_TABLE_NAME", "hardwarebugs")
         storage_queue_name = os.getenv("STORAGE_QUEUE_NAME", "bugmessages")
 
@@ -141,12 +146,14 @@ def create_message_timer(mytimer: func.TimerRequest) -> None:
             partition_key = entity.get("PartitionKey", "UnknownPartitionKey")
             row_key = entity.get("RowKey", "UnknownRowKey")
             bug_id = entity.get("BugId", "UnknownBugId")
+            joke = entity.get("Joke", "UnknownJoke")
 
             # create a message and send to queue
             message = {
                 "PartitionKey": partition_key,
                 "RowKey": row_key,
-                "BugId": bug_id
+                "BugId": bug_id,
+                "Joke": joke
             }
 
             # Encode the message as JSON and send to the queue
@@ -177,7 +184,7 @@ async def process_message(msg: func.QueueMessage):
             credential = DefaultAzureCredential()
 
             storage_table_name = os.getenv("STORAGE_TABLE_NAME", "hardwarebugs")
-            storage_account_name = os.getenv("STORAGE_ACCOUNT_NAME", "matrixmathconsumptionfun")
+            storage_account_name = os.getenv("STORAGE_ACCOUNT_NAME", "azdurablefunc001")
 
             table_service = TableServiceClient(
                 endpoint=f"https://{storage_account_name}.table.core.windows.net",
@@ -195,6 +202,7 @@ async def process_message(msg: func.QueueMessage):
             partition_key = message_dict.get("PartitionKey")
             row_key = message_dict.get("RowKey")
             bug_id = message_dict.get("BugId")
+            joke = message_dict.get("Joke")
             span.set_attribute("message.bug_id", bug_id)
 
             # Retrieve the entity from Table Storage
@@ -202,12 +210,27 @@ async def process_message(msg: func.QueueMessage):
 
             # Update the entity as "processed"
             entity["Status"] = "Processed"
+
+            # explain joke
+            time.sleep(10)
+            joke_context = summarize_text(joke)
+            entity["JokeContext"] = joke_context
+            entity["Updated_at"] = datetime.datetime.utcnow().isoformat()
+
             table_client.update_entity(entity, mode=UpdateMode.REPLACE)
             trace.get_current_span().add_event("Updated entity", {
                 "partition_key": partition_key,
                 "row_key": row_key,
-                "status": "Processed"
+                "status": "Processed",
+                "joke_context": joke_context
             })
+
+
+            # call the method to call open ai and summarize the text the text below
+            # text = "I recently discovered I could make fudge with just chocolate chips, sweetened condensed milk, vanilla extract, and a thick pot on slow heat. I tried it with dark chocolate chunks and I tried it with semi-sweet chocolate chips. It's better with both kinds. It comes out pretty bad with just the dark chocolate. The best add-ins are crushed almonds and marshmallows -- what you get from that is Rocky Road. It takes about twenty minutes from start to fridge, and then it takes about six months to work off the twenty pounds you gain from eating it. All things in moderation, friends. All things in moderation."
+            
+            
+           # summarize_text(text)
 
             span.set_attribute("entity.partition_key", partition_key)
             span.set_attribute("entity.row_key", row_key)
@@ -215,3 +238,144 @@ async def process_message(msg: func.QueueMessage):
         except Exception as e:
             span.record_exception(e)
             raise e
+
+# Function 4: Generate random messages and store in Table Storage
+@myApp.function_name(name="generate_random_messages")
+@myApp.timer_trigger(schedule="0 */1 * * * *", 
+              arg_name="mytimer",
+              run_on_startup=False) 
+def generate_random_messages(mytimer: func.TimerRequest) -> None:
+    """
+    Timer-triggered function that generates 500 unique and random messages per minute and stores them in Table Storage.
+    
+    Args:
+        mytimer (func.TimerRequest): The Timer request object.
+    """
+    with tracer.start_as_current_span("generate_random_messages", kind=SpanKind.INTERNAL) as span:
+        try:
+            # Use DefaultAzureCredential to authenticate with managed identity
+            credential = DefaultAzureCredential()
+
+            storage_account_name = os.getenv("STORAGE_ACCOUNT_NAME", "azdurablefunc001")
+            storage_table_name = os.getenv("STORAGE_TABLE_NAME", "hardwarebugs")
+            messages_per_minute = int(os.getenv("MESSAGES_PER_MINUTE", "5"))
+
+            table_service = TableServiceClient(
+                endpoint=f"https://{storage_account_name}.table.core.windows.net",
+                credential=credential
+            )
+            table_client = table_service.get_table_client(table_name=storage_table_name)
+
+            batch_operations = []
+            partition_key = str(uuid.uuid4())
+
+            for _ in range(messages_per_minute):
+                # partition_key = str(uuid.uuid4())
+                row_key = str(uuid.uuid4())
+                bug_id = str(random.randint(1, 99999))
+                joke = generate_random_joke()
+                span.set_attribute("message.bug_id", bug_id)
+
+                # Create a message and store in Table Storage
+                message = {
+                    "PartitionKey": partition_key,
+                    "RowKey": row_key,
+                    "BugId": bug_id,
+                    "Status": "New",
+                    "Joke": joke,
+                    "Created_at": datetime.datetime.utcnow().isoformat()
+                }
+
+                batch_operations.append(("create", message))
+                span.add_event("Prepared message for batch", {"message": json.dumps(message)})
+
+            table_client.submit_transaction(batch_operations)
+            span.add_event("Batch operation completed", {"batch_size": len(batch_operations)})
+            time.sleep(10)
+        except Exception as e:
+            span.record_exception(e)
+            raise e
+        
+def summarize_text(text: str) -> str:
+    """
+    Summarize the given text using Azure OpenAI service.
+
+    Args:
+        text (str): The text to be summarized.
+
+    Returns:
+        str: The summarized text.
+    """
+    with tracer.start_as_current_span("AzureOpenAI", kind=SpanKind.CLIENT) as span:  # Mark this as a dependency call
+        # Use DefaultAzureCredential to authenticate with managed identity
+        credential = DefaultAzureCredential()
+
+        # Assuming the OpenAI endpoint and deployment name are set as environment variables
+        openai_endpoint = os.getenv("OPENAI_ENDPOINT")
+        openai_deployment_name = os.getenv("OPENAI_DEPLOYMENT_NAME")
+
+        token_provider = get_bearer_token_provider(
+            credential, "https://cognitiveservices.azure.com/.default"
+        )
+
+        client = AzureOpenAI(
+            azure_endpoint=openai_endpoint,
+            azure_ad_token_provider=token_provider,
+            api_version= os.getenv("API_VERSION_GA", "2024-02-15-preview"),
+        )
+
+        prompt = f"Given this Kubernetes sarcastic joke, explain what the context is. Why do you think this joke was choose and what are we poking about Kubernetes complexity? Here is the joke: {text}"
+        span.set_attribute("openai.prompt", prompt)
+
+        response = client.chat.completions.create(
+            model=openai_deployment_name,
+            messages=[
+                {"role": "user", "content": prompt},
+            ],
+        )
+
+        summarized_text = response.choices[0].message.content
+        span.set_attribute("openai.summarized_text", summarized_text)
+
+        return summarized_text
+    
+
+def generate_random_joke():
+    """
+    Generate a random joke using Azure OpenAI service.
+    Returns:
+        str: A random joke.
+    """
+    with tracer.start_as_current_span("AzureOpenAI", kind=SpanKind.CLIENT) as span:  # Mark this as a dependency call
+        # Use DefaultAzureCredential to authenticate with managed identity
+        credential = DefaultAzureCredential()
+
+        # Assuming the OpenAI endpoint and deployment name are set as environment variables
+        openai_endpoint = os.getenv("OPENAI_ENDPOINT")
+        openai_deployment_name = os.getenv("OPENAI_DEPLOYMENT_NAME")
+
+        token_provider = get_bearer_token_provider(
+            credential, "https://cognitiveservices.azure.com/.default"
+        )
+
+        client = AzureOpenAI(
+            azure_endpoint=openai_endpoint,
+            azure_ad_token_provider=token_provider,
+            api_version= os.getenv("API_VERSION_GA", "2024-02-15-preview"),
+        )
+
+        prompt = "Give me a random and sarcastic joke about Kubernetes complexity. Something like: I would tell you a joke about Kubernetes, but it would probably require 5 different microservices to run."
+        span.set_attribute("openai.prompt", prompt)
+
+        response = client.chat.completions.create(
+            model=openai_deployment_name,
+            messages=[
+                {"role": "user", "content": prompt},
+            ],
+        )
+
+        joke_text = response.choices[0].message.content
+        span.set_attribute("openai.response", joke_text)
+
+        return  joke_text
+# Run the Durable Functions app
